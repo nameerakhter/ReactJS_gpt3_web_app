fc60e73f4157bc94185541f515fa53e174242b58
nameer akhter <akhatarnameer@gmail.com>
2023-07-19 23:34:45 +0530

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+import google from 'D:/gp3_web/src/assets/assets/google.png';
+import slack from 'D:/gp3_web/src/assets/assets/slack.png';
+import atlassian from 'D:/gp3_web/src/assets/assets/atlassian.png';
+import dropbox from 'D:/gp3_web/src/assets/assets/dropbox.png';
+import shopify from 'D:/gp3_web/src/assets/assets/shopify.png';
+
+
+export{
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
---
+++ src/containers/blog/import.js
@@ -0,0 +1,13 @@
+import blog01 from 'D:/gp3_web/src/assets/assets/blog01.png';
+import blog02 from 'D:/gp3_web/src/assets/assets/blog02.png';
+import blog03 from 'D:/gp3_web/src/assets/assets/blog03.png';
+import blog04 from 'D:/gp3_web/src/assets/assets/blog04.png';
+import blog05 from 'D:/gp3_web/src/assets/assets/blog05.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+}
